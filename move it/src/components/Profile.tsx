331ff62b8aaa2fa0fb97f0c0470e82fb1c41890e
anathayna <anathaynafranca@gmail.com>
2021-02-23 09:54:13 -0300

    import images & adding completed challenges

--- move it/src/components/Profile.tsx
+++ move it/src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/pages/Profile.module.css'
+import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
     return(
@@ -6,7 +6,10 @@ export function Profile() {
             <img src="https://github.com/anathayna.png" alt="anathayna"/>
             <div>
                 <strong>Ana Thayna</strong>
-                <p>level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    level 1
+                </p>
             </div>
         </div>
     );
