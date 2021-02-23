cf7b62ee21a3455fc6d60fcee0dd08b10d1ea30d
anathayna <anathaynafranca@gmail.com>
2021-02-23 00:07:50 -0300

    finish finish global.css

--- move it/src/App.tsx
+++ move it/src/App.tsx
@@ -1,18 +1,9 @@
-import { Button } from "./components/Button";
 import './styles/global.css';
 
 function App() {
   return (
     <div>
-      <Button color="red">
-        botão 1
-      </Button>
-      <Button color="green">
-        botão 2
-      </Button>
-      <Button color="blue">
-        botão 3
-      </Button>
+      <button style={{ height: '3rem' }}>teste</button>
     </div>
   );
 }
