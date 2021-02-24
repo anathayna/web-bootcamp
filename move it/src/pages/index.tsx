292580b55a8967f83f939d8a983668fadc859a9f
anathayna <anathaynafranca@gmail.com>
2021-02-23 22:45:20 -0300

    finish countdown button

--- move it/src/pages/index.tsx
+++ move it/src/pages/index.tsx
@@ -3,12 +3,15 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Countdown } from "../components/Countdown";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>inicío | move.it</title>
+      </Head>
       <ExperienceBar />
-
       <section>
         <div>
           <Profile />
